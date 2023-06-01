2f1326f9e7e4c7d8c9af84ad043317c48b7a4eda
liam-leny <liamleny@protonmail.com>
2023-06-01 20:43:52 +0200

    Separation of the form-filling stage

--- app-todo/scripts/main.js
+++ app-todo/scripts/main.js
@@ -1,68 +1,78 @@
 const app = Vue.createApp({
     data() {
-      return {
-        description: '',
-        dateDebut: '',
-        dateFin: '',
-        etat: '',
-        priorite: '',
-        taches: [] 
-      };
+        return {
+            description: '',
+            dateDebut: '',
+            dateFin: '',
+            etat: '',
+            priorite: '',
+            taches: [],
+            showForm: false,
+            showConfirmation: false,
+        };
     },
     methods: {
-      ajoutTache() {
-        
+        ajoutTache() {
 
-        // Vérifie que tous les champs du formulaire aient été remplis
-        if (
-            this.description.trim() === '' ||
-            this.dateDebut.trim() === '' ||
-            this.dateFin.trim() === '' ||
-            this.etat.trim() === '' ||
-            this.priorite.trim() === ''
-          ) {
-            alert('Veuillez remplir tous les champs du formulaire.');
-            return;
-          }
-        
-          // dateDebut < dateFin
-          const dateDebutObj = new Date(this.dateDebut);
-          const dateFinObj = new Date(this.dateFin);
-        
-          if (dateDebutObj >= dateFinObj) {
-            alert('La date de début doit être antérieure à la date de fin.');
-            return;
-          }
 
-        const newTask = {
-          description: this.description,
-          dateDebut: this.dateDebut,
-          dateFin: this.dateFin,
-          etat: this.etat,
-          priorite: this.priorite,
-          id: Date.now() // ID unique pour chaque tâche (utilisation de la timestamp actuelle)
-        };
-  
-        this.taches.push(newTask); // Ajouter la nouvelle tâche au tableau
-  
-        this.description = '';
-        this.dateDebut = '';
-        this.dateFin = '';
-        this.etat = '';
-        this.priorite = '';
-      },
-      suppressionTache(tacheId) {
-        this.taches = this.taches.filter(tache => tache.id !== tacheId);
-      },
-      suppressionToutesTaches() {
-        if (this.taches.length == 0) {
-            alert('Il y a aucune tache à supprimer.');
-        }
-        this.taches = [];
-      },
+            // Vérifie que tous les champs du formulaire aient été remplis
+            if (
+                this.description.trim() === '' ||
+                this.dateDebut.trim() === '' ||
+                this.dateFin.trim() === '' ||
+                this.etat.trim() === '' ||
+                this.priorite.trim() === ''
+            ) {
+                alert('Veuillez remplir tous les champs du formulaire.');
+                return;
+            }
+
+            // dateDebut < dateFin
+            const dateDebutObj = new Date(this.dateDebut);
+            const dateFinObj = new Date(this.dateFin);
+
+            if (dateDebutObj >= dateFinObj) {
+                alert('La date de début doit être antérieure à la date de fin.');
+                return;
+            }
+
+            const newTask = {
+                description: this.description,
+                dateDebut: this.dateDebut,
+                dateFin: this.dateFin,
+                etat: this.etat,
+                priorite: this.priorite,
+                id: Date.now() // ID unique pour chaque tâche (utilisation de la timestamp actuelle)
+            };
+
+            // Afficher la confirmation
+            this.showForm = false;
+            this.showConfirmation = true;
+
+            this.taches.push(newTask); // Ajouter la nouvelle tâche au tableau
+
+            this.description = '';
+            this.dateDebut = '';
+            this.dateFin = '';
+            this.etat = '';
+            this.priorite = '';
+        },
+        suppressionTache(tacheId) {
+            this.taches = this.taches.filter(tache => tache.id !== tacheId);
+        },
+        suppressionToutesTaches() {
+            if (this.taches.length == 0) {
+                alert('Il y a aucune tache à supprimer.');
+            }
+            this.taches = [];
+        },
+        resetForm() {
+            // Réinitialiser le formulaire et masquer la confirmation
+            this.showForm = false;
+            this.showConfirmation = false;
+          }
     }
-    
-  });
-  
-  app.mount('#app');
-  
+
+});
+
+app.mount('#app');
