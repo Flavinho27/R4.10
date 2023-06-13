48a293688ef0b8fcc8686ac9a02fb0d8d6863503
liam-leny <liamleny@protonmail.com>
2023-06-13 12:47:01 +0200

    Init site transformation using Vue components only

---
+++ app-todo-vue/src/components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav class="teal darken-3" role="navigation">
+      <div class="nav-wrapper container">
+        <a id="logo-container" href="#" class="btn" @click="showToutesTaches">
+          <img src="../app-todo/ressources/logo.png" alt="Logo" width="50rem" />
+        </a>
+        <ul class="right hide-on-med-and-down">
+          <li><a href="#">NavBar Link</a></li>
+        </ul>
+      </div>
+    </nav>
+  </template>
+  
+  <script>
+  export default {
+    // Composant NavBar
+  }
+  </script>
+  
---
+++ app-todo-vue/src/views/AboutView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="about">
+      <h1>About Page</h1>
+      <p>Welcome to the About page!</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'About',
+  }
+  </script>
+  
+  <style scoped>
+  .about {
+    margin: 20px;
+  }
+  </style>
+  
