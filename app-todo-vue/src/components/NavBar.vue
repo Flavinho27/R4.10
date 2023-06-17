bf9e20eb799ff9f7cfaddd61d51ffbf47c5fb698
liam-leny <liamleny@protonmail.com>
2023-06-17 18:36:26 +0200

    Add our website logo

--- app-todo-vue/src/components/NavBar.vue
+++ app-todo-vue/src/components/NavBar.vue
@@ -1,8 +1,8 @@
 <template>
     <nav class="teal darken-3" role="navigation">
       <div class="nav-wrapper container">
-        <a href="index.html" @click="showToutesTaches">
-          <img src="icons/logo.png" alt="Logo" width="50rem" class="logo"/>
+        <a href="index.html">
+          <img src="./icons/logo.png" alt="Logo" class="logo"/>
         </a>
         <ul class="right">
           <li><a href="../../aide.html">Aide</a></li>
@@ -12,16 +12,11 @@
   </template>
   
   <script>
-  export default {
-    methods: {
-      showToutesTaches() {
-        // Logique pour afficher toutes les tâches
-      }
-    }
-  }
   </script>
-  
   <style scoped>
-  /* Styles CSS spécifiques au composant Navbar */
-  </style>
+   .logo {
+    width: 3.5rem;
+    margin-top: 10px;
+  }
+</style>
   
--- app-todo-vue/src/main.js
+++ app-todo-vue/src/main.js
@@ -1,9 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import TasksTable from './components/TasksTable.vue';
 
 const app = createApp(App);
-
-app.component('TasksTable', TasksTable);
 
 app.mount('#app');
